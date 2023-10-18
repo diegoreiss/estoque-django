10535d260043a3a625b50e1566a34e47a6bdd2a3
diegoreiss <99758941+diegoreiss@users.noreply.github.com>
2023-10-18 19:06:35 -0300

    django admin features

---
+++ estoque/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Products
+
+# Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'size', 'in_stock']
+    list_filter = ['in_stock']
+    list_editable = ['size']
+    search_fields = ['name']
+
+admin.site.register(Products, ProductsAdmin)
---
+++ estoque/models.py
@@ -0,0 +1,25 @@
+from typing import Any
+from django.db import models
+
+# Create your models here.
+
+class Products(models.Model):
+    itens = [
+        ('1', 'P'),
+        ('2', 'M'),
+        ('3', 'G'),
+        ('4', 'GG')
+    ]
+
+    name = models.CharField(max_length=255)
+    cod = models.IntegerField(unique=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField()
+    qtd = models.IntegerField()
+    size = models.CharField(choices=itens, max_length=255)
+    discount = models.IntegerField()
+    created_at = models.DateField() 
+    in_stock = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
